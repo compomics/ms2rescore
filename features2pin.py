32a10c290b008c8786129a366351012dfa956e5b
anasilviacs <anascsilva@vib-ugent.be>
2017-09-01 09:49:47 +0200

    Added argument parser

--- features2pin.py
+++ features2pin.py
@@ -1,11 +1,23 @@
+"""
+prepare pin files with the ms2pip and percolator features
+"""
 import pandas as pd
+import argparse
+
+# Arguments: percolator features, .mzid
+parser = argparse.ArgumentParser(description="Get a pin file with Percolator and ms2pip features")
+parser.add_argument('-t', dest='targets', help='Path to target search ms2pip features file')
+parser.add_argument('-d', dest='decoys', help='Path to decoy search ms2pip features file')
+parser.add_argument('-p', dest='pin', help='Path to pin file')
+
+args = parser.parse_args()
 
 # Read rescore_features.csv file and fillna
-rescore_targets = pd.read_csv('../pyrococcus/pepfile_targets_rescore_features.csv')
+rescore_targets = pd.read_csv(args.targets)
 rescore_targets = rescore_targets.fillna(0)
 
 # If not concat searches, do that for target and decoy files
-rescore_decoys = pd.read_csv('../pyrococcus/pepfile_decoys_rescore_features.csv')
+rescore_decoys = pd.read_csv(args.decoys)
 rescore_decoys = rescore_decoys.fillna(0)
 
 # join target and decoy tables
@@ -13,7 +25,7 @@ all_features = pd.concat([rescore_decoys.merge(pin[pin.Label == -1], left_on='sp
                           rescore_targets.merge(pin[pin.Label == 1], left_on='spec_id', right_on='TITLE')])
 
 # read pin file
-pin = pd.read_csv('../pyrococcus/msgf_features.pin', sep='\t')
+pin = pd.read_csv(args.pin, sep='\t')
 
 # columns to save
 rescore_features = list(all_features.columns[4:42])
@@ -21,8 +33,8 @@ percolator_features = list(all_features.columns[45:-5])
 percolator_default = percolator_features[:27]
 
 # Writing files with appropriate columns
-all_features[['SpecId', 'Label', 'ScanNr'] + rescore_features + ['Peptide', 'Proteins']].to_csv('../pyrococcus/only_rescore.pin', sep='\t', index=False)
-all_features[['SpecId', 'Label', 'ScanNr'] + percolator_features + ['Peptide', 'Proteins']].to_csv('../pyrococcus/all_percolator.pin', sep='\t', index=False)
-all_features[['SpecId', 'Label', 'ScanNr'] + percolator_default + ['Peptide', 'Proteins']].to_csv('../pyrococcus/percolator_default.pin', sep='\t', index=False)
-all_features[['SpecId', 'Label', 'ScanNr'] + percolator_features + rescore_features + ['Peptide', 'Proteins']].to_csv('../pyrococcus/all_features.pin', sep='\t', index=False)
-all_features[['SpecId', 'Label', 'ScanNr'] + percolator_default + rescore_features + ['Peptide', 'Proteins']].to_csv('../pyrococcus/default_plus_rescore.pin', sep='\t', index=False)
+all_features[['SpecId', 'Label', 'ScanNr'] + rescore_features + ['Peptide', 'Proteins']].to_csv('{}_only_rescore.pin'.format(args.pin.split('.')[:-1]), sep='\t', index=False)
+all_features[['SpecId', 'Label', 'ScanNr'] + percolator_features + ['Peptide', 'Proteins']].to_csv('{}_all_percolator.pin'.format(args.pin.split('.')[:-1]), sep='\t', index=False)
+all_features[['SpecId', 'Label', 'ScanNr'] + percolator_default + ['Peptide', 'Proteins']].to_csv('{}_percolator_default.pin'.format(args.pin.split('.')[:-1]), sep='\t', index=False)
+all_features[['SpecId', 'Label', 'ScanNr'] + percolator_features + rescore_features + ['Peptide', 'Proteins']].to_csv('{}_all_features.pin'.format(args.pin.split('.')[:-1]), sep='\t', index=False)
+all_features[['SpecId', 'Label', 'ScanNr'] + percolator_default + rescore_features + ['Peptide', 'Proteins']].to_csv('{}_default_and_rescore.pin'.format(args.pin.split('.')[:-1]), sep='\t', index=False)
