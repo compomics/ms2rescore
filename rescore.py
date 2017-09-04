d685b0687766e64adec2e87b58ecaa3f5a4986da
anasilviacs <anascsilva@vib-ugent.be>
2017-09-04 15:39:22 +0200

    Tentatively better filenames

--- rescore.py
+++ rescore.py
@@ -11,6 +11,8 @@ from mapper import mapper
 import pin2pepfile
 import features2pin
 
+# TODO better file names
+# TODO call ms2pip and Percolator from this script
 # Path to MSGFPlus - this should come from a config file
 MSGF_DIR = "/home/compomics/software/MSGFPlus"
 
@@ -28,28 +30,30 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Run MSGF+
-    mgf_msgf2pin.run_msgfplus(MSGF_DIR, args.spec_file + ".target", args.spec_file,
+    mgf_msgf2pin.run_msgfplus(MSGF_DIR, args.spec_file + ".msgf_out", args.spec_file,
                  args.fasta_file, args.modsfile, args.frag)
 
     # Convert .mzid to pin, for percolator. XXX is the decoy pattern from MSGF+
     convert_command = "msgf2pin -P XXX %s.mzid > %s.pin" % (
-        args.spec_file + ".target", args.spec_file + ".target")
+        args.spec_file + ".msgf_out", args.spec_file + ".msgf_out")
     sys.stdout.write("Converting .mzid file to pin file: {}".format(convert_command))
     sys.stdout.flush()
     subprocess.run(convert_command, shell=True)
 
     # Add mgf TITLE column to pin file
-    pin = mapper.lazy_pin_parser(args.spec_file + ".target.pin")
-    pin = mapper.map_mgf_title(pin, args.spec_file + ".target")
+    pin = mapper.lazy_pin_parser(args.spec_file + ".msgf_out.pin")
+    pin = mapper.map_mgf_title(pin, args.spec_file + ".msgf_out")
 
-    pin.to_csv(args.spec_file + ".target.titles.pin", sep='\t', index=False)
+    # Write pin file
+    pin.to_csv(args.spec_file + ".titles.pin", sep='\t', index=False)
 
-    peprec = pin2pepfile.make_pepfile(args.spec_file + ".target.titles.pin")
-    pin2pepfile.write_PEPREC(peprec, args.spec_file + ".target.titles.pin")
+    # Create & write PEPREC file from the pin file
+    peprec = pin2pepfile.make_pepfile(args.spec_file + ".titles.pin")
+    pin2pepfile.write_PEPREC(peprec, args.spec_file + ".titles.pin")
 
     # Run ms2pip_rescore
 
-    features = join_features(args.pep_file + '.target.titles.pin.PEPREC_rescore_features.csv', args.pin)
+    features = join_features(args.pep_file + '.titles.pin.PEPREC_rescore_features.csv', args.pin)
     write_pin_files(features)
 
     # Run Percolator
