3414e437746c7c9831d3ebcda2cf8c6b809ddef7
anasilviacs <anascsilva@vib-ugent.be>
2017-09-05 12:07:59 +0200

    Renaming files and combining all functions in one py file

--- rescore.py
+++ rescore.py
@@ -1,66 +1,180 @@
 """
-Module that calls the necessary functions and run the re-scoring algorithm.
+Functions necessary to run the rescore algorithm. Currently supports MSGF+ with
+concatenated searches.
 """
 
-import argparse
-import sys
 import subprocess
+import sys
+import argparse
+import re
+import pandas as pd
 
-from mapper import mapper
-# TODO join all these guys in one py file
-import mgf_msgf2pin
-import pin2pepfile
-import features2pin
+from mapper import mapper #TODO shouldn't have to do this, check __init__.py
 
-# TODO better file names
-# TODO call Percolator from this script
-# TODO ms2pip config file - set a default?
-# Path to MSGFPlus & ms2pip - these should come from a config file
-MSGF_DIR = "/home/compomics/software/MSGFPlus"
-MS2PIP_DIR = "ms2pip_c"
+def run_msgfplus(msgf_dir, outfile, mgffile, fastafile, modsfile, frag='HCD'):
+    """
+    Runs MSGF+ with some fixed settings: 10ppm precursor mass tolerance,
+    concatenated search, minimum peptide length of 8aa, minimum charge is 2 and
+    maximum is 4, 1 match per spectrum, include additional features, no protocol
+    and use 23 threads. MSGF+ is called with subprocess.
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Run MSGF+ and Percolator')
-    parser.add_argument('spec_file', metavar='spectrum-file',
-                        help='file containing MS2 spectra (MGF,PKL,DTA,mzXML,mzDATA or mzML)')
-    parser.add_argument('fasta_file', metavar='FASTA-file',
-                        help='file containing protein sequences')
-    parser.add_argument('-m', '--mods', metavar='FILE', action="store", default='',
-                        dest='modsfile', help='Mods.txt file for MSGF+')
-    parser.add_argument('-f', '--frag', metavar='frag_method', action="store", default='HCD',
-                        dest='frag', help='fragmentation method (CID or HCD), default HCD')
+    :param msgfdir: string, path to MSGFPlus.jar
+    :param outfile: string, path where to store the search results (.mzid)
+    :param mgffile: string, spectrum file (MGF,PKL,DTA,mzXML,mzDATA or mzML)
+    :param fastafile: string, the database to search against in .fasta format
+    :param modsfile: string, path to the file with modifications
+    :param frag: fragmentation method (HCD or CID) on which some settings depend
+    """
+    if frag == 'HCD':
+        m = 3
+        inst = 1
+    elif frag == 'CID':
+        m = 1
+        inst = 0
+    if modsfile != '':
+        mods = '-mod {} '.format(modsfile)
+    else:
+        mods = ''
+    msgf_command = "java -Xmx8000M -jar {}/MSGFPlus.jar {}-s {} -d {} -o {} -t \
+        10ppm -tda 1 -m {} -inst {} -minLength 8 -minCharge 2 -maxCharge 4 -n \
+        1 -addFeatures 1 -protocol 0 -thread 23 \n".format(msgf_dir, mods,
+        mgffile, fastafile, outfile + ".mzid", m, inst)
 
-    args = parser.parse_args()
-
-    # Run MSGF+
-    mgf_msgf2pin.run_msgfplus(MSGF_DIR, args.spec_file + ".msgf_out", args.spec_file,
-                 args.fasta_file, args.modsfile, args.frag)
-
-    # Convert .mzid to pin, for percolator. XXX is the decoy pattern from MSGF+
-    convert_command = "msgf2pin -P XXX %s.mzid > %s.pin" % (
-        args.spec_file + ".msgf_out", args.spec_file + ".msgf_out")
-    sys.stdout.write("Converting .mzid file to pin file: {}".format(convert_command))
+    sys.stdout.write("Running search with MSGF+: {}".format(msgf_command))
     sys.stdout.flush()
-    subprocess.run(convert_command, shell=True)
+    subprocess.run(msgf_command, shell=True)
 
-    # Add mgf TITLE column to pin file
-    pin = mapper.lazy_pin_parser(args.spec_file + ".msgf_out.pin")
-    pin = mapper.map_mgf_title(pin, args.spec_file + ".msgf_out")
+    return None
 
-    # Write pin file
-    pin.to_csv(args.spec_file + ".titles.pin", sep='\t', index=False)
+def make_pepfile(path_to_pin, modsfile=None):
+    """
+    Read a pin file and create the corresponding pepfile dataframe, which will
+    be saved as a MS2PIP PEPREC file.
 
-    # Create & write PEPREC file from the pin file
-    peprec = pin2pepfile.make_pepfile(args.spec_file + ".titles.pin")
-    pin2pepfile.write_PEPREC(peprec, args.spec_file + ".titles.pin")
+    :param path_to_pin: path to pin file
 
-    # Run ms2pip_rescore
-    ms2pip_command = "python {}/ms2pipC.py {} -c {someconfigfile} -s {}".format(MS2PIP_DIR, args.spec_file + ".titles.pin.PEPREC", config, args.spec_file, -R)
-    sys.stdout.write("Running ms2pip with the rescore option: {}".format(ms2pip_command))
-    sys.stdout.flush()
-    subprocess.run(ms2pip_command, shell=True)
+    Returns
+    :pd.DataFrame pepfile, columns ['TITLE', 'Peptide', 'Charge', 'Label',
+        'modifications']
+    """
+    pin = mapper.lazy_pin_parser(path_to_pin)
 
-    features = join_features(args.pep_file + '.titles.pin.PEPREC_rescore_features.csv', args.pin)
-    write_pin_files(features)
+    pin.loc[:, 'Charge'] = [2 if r[1].Charge2 == '1' else 3 if r[1].Charge3 == '1' else 4 if r[1].Charge4 == '1' else 5 if r[1].Charge5 == '1' else 6 if r[1].Charge6 == '1' else 0 for r in pin.iterrows()]
 
-    # Run Percolator
+    pepfile = pin[['TITLE', 'Peptide', 'Charge', 'Label']]
+
+    # Add modifications column to PEPREC file
+    # TODO: read modifications from MSGF+ modifications file
+    # the keys correspond to the UNIMOD keys for each modification
+    modifications = {'4': 'Cam', '35': 'Oxidation'}
+
+    modlist = []
+    for _, row in pepfile.iterrows():
+        if 'UNIMOD' in row['Peptide']:
+            pep = row['Peptide'].split('.')[1]
+            mods = re.findall(r'\[([^]]*)\]', pep)
+            modstring = ''
+            for mod in mods:
+                mod = '[' + mod + ']'
+                modstring += str(pep.find(mod)) + '|' + modifications[mod.split(':')[1].rstrip(']')] + '|'
+                pep = pep.replace(mod, '', 1)
+            modlist.append(modstring.rstrip('|'))
+        else:
+            modlist.append('')
+    pepfile.loc[:, 'modifications'] = modlist
+
+    # Rewrite peptide sequences without the UNIMOD modification ids
+    pep_list = []
+    for _, row in pepfile.iterrows():
+        pep = row['Peptide']
+        pep = pep.split('.')[1]
+        if 'UNIMOD' in pep:
+            mods = re.findall(r'\[([^]]*)\]', pep)
+            for mod in mods:
+                pep = pep.replace('[' + mod + ']', '', 1)
+        pep_list.append(pep)
+    pepfile.loc[:, 'peptide'] = pep_list
+
+    return pepfile
+
+def write_PEPREC(pepfile, path_to_pep, concat=True):
+    """
+    Write the PEPREC file, which will be the input to MS2PIP.
+
+    :param pepfile: pd.DataFrame created by rescore.make_pepfile()
+    :param path_to_pep: string, path where to save the PEPREC file(s)
+    :param concat: boolean, True if the search was concatenated
+    """
+
+    if concat:
+        pepfile_tosave = pepfile[['TITLE', 'modifications', 'peptide', 'Charge', 'Label']]
+        pepfile_tosave.columns = ['spec_id', 'modifications', 'peptide', 'charge', 'Label']
+        pepfile_tosave.to_csv(path_to_pep + '.PEPREC', sep=' ', index=False)
+
+    else:
+        pepfile_tosave = pepfile[pepfile.Label == 1][['TITLE', 'modifications', 'peptide', 'Charge', 'Label']]
+        pepfile_tosave.columns = ['spec_id', 'modifications', 'peptide', 'charge', 'Label']
+        pepfile_tosave.to_csv(path_to_pep + '.targets.PEPREC', sep=' ', index=False)
+
+        pepfile_tosave = pepfile[pepfile.Label == -1][['TITLE', 'modifications', 'peptide', 'Charge', 'Label']]
+        pepfile_tosave.columns = ['spec_id', 'modifications', 'peptide', 'charge', 'Label']
+        pepfile_tosave.to_csv(path_to_pep + '.decoys.PEPREC', sep=' ', index=False)
+
+    return None
+
+def join_features(path_to_target_features, path_to_pin, path_to_decoy_features=None):
+    """
+    Combine the features table obtained from Percolator (i.e. in the pin file)
+    and the ones obtained from MS2PIP_rescore in one DataFrame
+
+    :param path_to_target_features: string, path to MS2PIP features
+    :param path_to_pin: string, path to pin file
+    :param path_to_decoy_features: string, if non-concatenated search then there
+         will be a separate feature file with the decoy features
+
+    Returns
+    :pd.DataFrame all_features, includes all the Percolator and MS2PIP features
+    """
+    # read pin file - should not need the lazy pin parser as this pin already has the TITLE which means it was processed by mapper
+    pin = pd.read_csv(path_to_pin, sep='\t')
+
+    # Read rescore_features.csv file and fillna
+    rescore_targets = pd.read_csv(path_to_target_features)
+    rescore_targets = rescore_targets.fillna(0)
+
+    # If not concat searches, do that for target and decoy files
+    if path_to_decoy_features != None:
+        rescore_decoys = pd.read_csv(args.decoys)
+        rescore_decoys = rescore_decoys.fillna(0)
+
+        # join target and decoy tables
+        all_features = pd.concat([rescore_decoys.merge(pin[pin.Label == -1], left_on='spec_id', right_on='TITLE'), rescore_targets.merge(pin[pin.Label == 1], left_on='spec_id', right_on='TITLE')])
+    else:
+        all_features = rescore_targets.merge(pin, left_on='spec_id', right_on='TITLE')
+
+    return all_features
+
+def write_pin_files(all_features, savepath):
+    """
+    Given a dataframe with all the features, writes five different pin files:
+    _only_rescore.pin with only the rescore features
+    _all_percolator.pin with all the percolator features
+    _percolator_default.pin with only the default percolator features
+    _all_features.pin with all the rescore and percolator features
+    _default_and_rescore.pin with the rescore and the default percolator features
+
+    :param all_features: pd.DataFrame obtained from rescore.join_features()
+    """
+    # columns to save
+    rescore_features = list(all_features.columns[4:42])
+    percolator_features = list(all_features.columns[45:-5])
+    percolator_default = percolator_features[:27]
+
+    # Writing files with appropriate columns
+    all_features[['SpecId', 'Label', 'ScanNr'] + rescore_features + ['Peptide', 'Proteins']].to_csv('{}_only_rescore.pin'.format(savepath), sep='\t', index=False)
+    all_features[['SpecId', 'Label', 'ScanNr'] + percolator_features + ['Peptide', 'Proteins']].to_csv('{}_all_percolator.pin'.format(savepath), sep='\t', index=False)
+    all_features[['SpecId', 'Label', 'ScanNr'] + percolator_default + ['Peptide', 'Proteins']].to_csv('{}_percolator_default.pin'.format(savepath), sep='\t', index=False)
+    all_features[['SpecId', 'Label', 'ScanNr'] + percolator_features + rescore_features + ['Peptide', 'Proteins']].to_csv('{}_all_features.pin'.format(savepath), sep='\t', index=False)
+    all_features[['SpecId', 'Label', 'ScanNr'] + percolator_default + rescore_features + ['Peptide', 'Proteins']].to_csv('{}_default_and_rescore.pin'.format(savepath), sep='\t', index=False)
+
+    return None
