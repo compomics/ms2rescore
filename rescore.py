65a14cf11c1e4128ce7b8bb0579364c6fe24c9b9
anasilviacs <anascsilva@vib-ugent.be>
2018-06-26 14:34:54 +0200

    Removed unused functions and corrected ms2pip config

--- rescore.py
+++ rescore.py
@@ -14,163 +14,18 @@ import seaborn as sns
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
 
-def run_msgfplus(msgf_dir, mgffile, fastafile, options):
-    """
-    Runs MSGF+ with some fixed settings: 10ppm precursor mass tolerance,
-    concatenated search, minimum peptide length of 8aa, minimum charge is 2 and
-    maximum is 4, 1 match per spectrum, include additional features, no protocol
-    and use 23 threads. MSGF+ is called with subprocess.
-
-    :param msgfdir: string, path to MSGFPlus.jar
-    :param mgffile: string, spectrum file (MGF,PKL,DTA,mzXML,mzDATA or mzML)
-    :param fastafile: string, the database to search against in .fasta format
-    :param options: dictionary, contains search engine options
-    """
-
-    if options["frag"] == 'HCD':
-        m = 3
-        inst = 1
-    elif options["frag"] == 'CID':
-        m = 1
-        inst = 0
-    if options["path_to_modsfile"] != '':
-        mods = '-mod {} '.format(options["path_to_modsfile"])
-    else:
-        mods = ''
-
-    outfile = mgffile.rstrip(".mgf") + ".mzid"
-
-    msgf_command = "java -Xmx28000M -jar {}/MSGFPlus.jar {}-s {} -d {} -o {} -t\
-         10ppm -tda 1 -m {} -inst {} -minLength {} -minCharge {} -maxCharge {}\
-         -n 1 -addFeatures 1 -protocol 0 -thread 23 > {}.log".format(msgf_dir,
-         mods, mgffile, fastafile, outfile, m, inst, options["min_length"],
-         options["min_charge"], options["max_charge"], mgffile.rstrip(".mgf"))
-
-    sys.stdout.write("Running search with MSGF+: {}".format(msgf_command))
-    sys.stdout.flush()
-    subprocess.run(msgf_command, shell=True)
-
-    return None
-
-
-def make_pepfile(path_to_pin):
-    """
-    Read a pin file and create the corresponding pepfile dataframe, which will
-    be saved as a MS2PIP PEPREC file.
-
-    :param path_to_pin: path to pin file
-
-    Returns
-    :pd.DataFrame pepfile, columns ['TITLE', 'Peptide', 'Charge', 'Label',
-        'modifications']
-    """
-
-    def parse_mods(modseq):
-        """
-        function to parse modifications from pin file and write them in
-        PEPREC column
-        """
-        # TODO additional modifications must be added here
-        # the keys correspond to the UNIMOD keys for each modification
-        modifications = {'1': 'Acetylation',
-                      '4': 'Cam',
-                      '35': 'Oxidation',
-                      '21': 'Phospho'}
-        if 'UNIMOD' in modseq:
-            pep = modseq.split('.')[1]
-            mods = re.findall(r'\[([^]]*)\]', pep)
-            modstring = ''
-            for mod in mods:
-                mod = '[' + mod + ']'
-                key = mod.split(':')[1].rstrip(']')
-                try:
-                    if key == '21':
-                        phospholoc = pep[pep.find(mod)-1]
-                        modstring += str(pep.find(mod)) + '|' + modifications[key] + phospholoc + '|'
-                        pep = pep.replace(mod, '', 1)
-                    else:
-                        modstring += str(pep.find(mod)) + '|' + modifications[key] + '|'
-                        pep = pep.replace(mod, '', 1)
-                except:
-                        print('Modification not expected: {}'.format(mod))
-            modstring = modstring.rstrip('|')
-        else:
-            modstring = ''
-        return modstring
-
-    def parse_pepseq(modseq):
-        """
-        function to parse unmodified peptide sequence from pin file and write
-        to PEPREC file
-        """
-        pep = modseq.split('.')[1]
-        if 'UNIMOD' in pep:
-            mods = re.findall(r'\[([^]]*)\]', pep)
-            for mod in mods:
-                pep = pep.replace('[' + mod + ']', '', 1)
-        return pep
-
-    pin = pd.read_csv(path_to_pin, sep='\t', header=0, skiprows=[1])
-
-    charge_states = []
-    for a in pin.columns:
-        if a.startswith('Charge'): charge_states.append(a)
-        else: continue
-
-    pin.loc[:, 'Charge'] = [None] * len(pin)
-
-    for ch in charge_states:
-        value = int(ch.lstrip('Charge'))
-        pin.loc[pin[ch]==1, 'Charge'] = value
-
-    pepfile = pin.loc[:, ['TITLE', 'Peptide', 'Charge', 'Label']]
-
-    # Add modifications column to PEPREC file
-    pepfile.loc[:, 'modifications'] = pepfile.Peptide.apply(parse_mods)
-    pepfile.modifications.fillna('', inplace=True)
-
-    # Rewrite peptide sequences without the UNIMOD modification ids
-    pepfile.loc[:, 'peptide'] = pepfile.Peptide.apply(parse_pepseq)
-
-    write_PEPREC(pepfile, path_to_pin)
-
-def write_PEPREC(pepfile, path_to_pep, concat=True):
-    """
-    Write the PEPREC file, which will be the input to MS2PIP.
-
-    :param pepfile: pd.DataFrame created by rescore.make_pepfile()
-    :param path_to_pep: string, path where to save the PEPREC file(s)
-    :param concat: boolean, True if the search was concatenated
-    """
-
-    if concat:
-        pepfile_tosave = pepfile.loc[:, ['TITLE', 'modifications', 'peptide', 'Charge', 'Label']]
-        pepfile_tosave.columns = ['spec_id', 'modifications', 'peptide', 'charge', 'Label']
-        pepfile_tosave.to_csv(path_to_pep + '.PEPREC', sep=' ', index=False)
-
-    else:
-        pepfile_tosave = pepfile.loc[pepfile.Label == 1, ['TITLE', 'modifications', 'peptide', 'Charge', 'Label']]
-        pepfile_tosave.columns = ['spec_id', 'modifications', 'peptide', 'charge', 'Label']
-        pepfile_tosave.to_csv(path_to_pep + '.targets.PEPREC', sep=' ', index=False)
-
-        pepfile_tosave = pepfile.loc[pepfile.Label == -1, ['TITLE', 'modifications', 'peptide', 'Charge', 'Label']]
-        pepfile_tosave.columns = ['spec_id', 'modifications', 'peptide', 'charge', 'Label']
-        pepfile_tosave.to_csv(path_to_pep + '.decoys.PEPREC', sep=' ', index=False)
-
-    return None
-
 def make_ms2pip_config(options):
     """
     write configuration file for ms2pip based on what's on the rescore config
     file.
     """
 
-    ms2pip_config = open(options["dir"] + "ms2pip.config", 'wt')
+    ms2pip_config = open(options["ms2pip"]["dir"] + "ms2pip.config", 'wt')
 
-    if options["frag"] == "CID":
+    if options["ms2pip"]["frag"] == "CID":
         ms2pip_config.write("frag_method=CID\n")
         ms2pip_config.write("frag_error=0.8\n")
-    elif options["frag"] == "phospho":
+    elif options["ms2pip"]["frag"] == "phospho":
         ms2pip_config.write("frag_method=phospho\n")
         ms2pip_config.write("frag_error=0.02\n")
     else:
@@ -360,37 +215,19 @@ def calculate_features(path_to_pred_and_emp, path_to_out):
     all_results.to_csv(path_to_out, index=False)
 
 
-def join_features(path_to_target_features, path_to_pin, path_to_decoy_features=None):
+def norm_features(path_to_features):
     """
-    Combine the features table obtained from Percolator (i.e. in the pin file)
-    and the ones obtained from MS2PIP_rescore in one DataFrame
+    Normalize the features obtained from MS2PIP
 
-    :param path_to_target_features: string, path to MS2PIP features
-    :param path_to_pin: string, path to pin file
-    :param path_to_decoy_features: string, if non-concatenated search then there
-         will be a separate feature file with the decoy features
+    :param path_to_features: string, path to MS2PIP features
 
     Returns
-    :pd.DataFrame all_features, includes all the Percolator and MS2PIP features
+    :pd.DataFrame features, includes normalized MS2PIP features
     """
-    # read pin file - should not need the lazy pin parser as this pin already
-    # has the TITLE which means it was processed by mapper
-    pin = pd.read_csv(path_to_pin, sep='\t')
 
-    # Read rescore_features.csv file and fillna
-    rescore_targets = pd.read_csv(path_to_target_features)
-    rescore_targets = rescore_targets.fillna(0)
-    # If not concat searches, do that for target and decoy files
-    if path_to_decoy_features != None:
-        rescore_decoys = pd.read_csv(path_to_decoy_features)
-        rescore_decoys = rescore_decoys.fillna(0)
-
-        # join target and decoy tables
-        all_features = pd.concat([rescore_decoys.merge(pin[pin.Label == -1], left_on='spec_id', right_on='TITLE'),
-                                  rescore_targets.merge(pin[pin.Label == 1], left_on='spec_id', right_on='TITLE')])
-    else:
-        all_features = rescore_targets.merge(pin, left_on='spec_id', right_on='TITLE')
-    all_features = all_features.drop(all_features.loc[:,all_features.columns.str.endswith('.1')], axis=1)
+    # Read features csv file and fillna
+    features = pd.read_csv(path_to_features)
+    features = features.fillna(0)
 
     norm_cols = ['spec_pearson_norm', 'ionb_pearson_norm', 'iony_pearson_norm',
         'spec_spearman_norm', 'ionb_spearman_norm', 'iony_spearman_norm',
@@ -416,34 +253,16 @@ def join_features(path_to_target_features, path_to_pin, path_to_decoy_features=N
         'iony_mean_abs_diff', 'iony_std_abs_diff', 'dotprod', 'dotprod_ionb',
         'dotprod_iony', 'cos', 'cos_ionb', 'cos_iony']
 
-    norm_features = pd.DataFrame(StandardScaler().fit_transform(X=all_features.loc[:, norm_cols]), columns=norm_cols)
-    all_features.loc[:, norm_features.columns] = norm_features
-    all_features.to_csv(path_to_pin.rstrip('.pin')+'_all_features.csv', sep=',', index=False)
+    norm_features = pd.DataFrame(StandardScaler().fit_transform(X=features.loc[:, norm_cols]), columns=norm_cols)
+    features.loc[:, norm_features.columns] = norm_features
+    features.to_csv(path_to_features, sep=',', index=False)
 
-
-def write_pin_files(path_to_features, savepath):
+def write_pin_files(path_to_features, path_to_pep, savepath):
     """
-    Given a dataframe with all the features, writes three different pin files:
-    _only_rescore.pin with only the rescore features
-    _all_percolator.pin with all the percolator features
-    _all_features.pin with all the rescore and percolator features
-
-    :param path_to_features: pd.DataFrame obtained from rescore.join_features()
-    :param savepath: path to save the pin files
+    Given a dataframe with all the features, writes a pin file
     """
-    # columns to save
-    percolator_features = ['ExpMass', 'CalcMass', 'RawScore', 'DeNovoScore',
-        'ScoreRatio', 'Energy', 'lnEValue', 'IsotopeError',
-        'lnExplainedIonCurrentRatio', 'lnNTermIonCurrentRatio',
-        'lnCTermIonCurrentRatio', 'lnMS2IonCurrent', 'Mass', 'PepLen', 'dM',
-        'absdM', 'MeanErrorTop7', 'sqMeanErrorTop7', 'StdevErrorTop7',
-        'Charge2', 'Charge3', 'Charge4', 'Charge5', 'Charge6', 'enzN', 'enzC',
-        'enzInt', 'ptm', 'A-Freq', 'C-Freq', 'D-Freq', 'E-Freq', 'F-Freq',
-        'G-Freq', 'H-Freq', 'I-Freq', 'K-Freq', 'L-Freq', 'M-Freq', 'N-Freq',
-        'P-Freq', 'Q-Freq', 'R-Freq', 'S-Freq', 'T-Freq', 'V-Freq', 'W-Freq',
-        'Y-Freq', 'B-Freq', 'Z-Freq', 'J-Freq', 'X-Freq', 'U-Freq', 'O-Freq']
-
-    rescore_features = ['spec_pearson_norm', 'ionb_pearson_norm',
+    # feature columns
+    features = ['spec_pearson_norm', 'ionb_pearson_norm',
         'iony_pearson_norm', 'spec_spearman_norm', 'ionb_spearman_norm',
         'iony_spearman_norm', 'spec_mse_norm', 'ionb_mse_norm', 'iony_mse_norm',
          'min_abs_diff_iontype_norm', 'max_abs_diff_iontype_norm',
@@ -470,65 +289,17 @@ def write_pin_files(path_to_features, savepath):
         'cos_ionb', 'cos_iony']
 
     all_features = pd.read_csv(path_to_features, sep=',')
-    # Writing files with appropriate columns
-    all_features.loc[:, ['SpecId', 'Label', 'ScanNr'] + rescore_features + ['Peptide', 'Proteins']].fillna(value=0).to_csv('{}_rescore.pin'.format(savepath), sep='\t', index=False)
-    all_features.loc[:, ['SpecId', 'Label', 'ScanNr'] + percolator_features + ['Peptide', 'Proteins']].fillna(value=0).to_csv('{}_percolator.pin'.format(savepath), sep='\t', index=False)
-    all_features.loc[:, ['SpecId', 'Label', 'ScanNr'] + percolator_features + rescore_features + ['Peptide', 'Proteins']].fillna(value=0).to_csv('{}_all_features.pin'.format(savepath), sep='\t', index=False)
+
+    pep = pd.read_csv(path_to_pep, sep=' ')
+    complete_df = pd.merge(all_features, pep, on=['spec_id', 'charge'])
+    complete_df.rename(mapper={'spec_id': 'SpecId', 'peptide': 'Peptide'}, axis='columns', inplace=True)
+
+    # Add artificial protein column, scan numbers and flanking aminoacids to
+    # peptide sequences
+    complete_df['ScanNr'] = list(range(len(complete_df)))
+    complete_df['Proteins'] = complete_df['Peptide']
+    complete_df['Peptide'] = 'X.' + complete_df.Peptide + '.X'
+
+    # Writing files with ordered columns
+    complete_df.loc[:, ['SpecId', 'Label', 'ScanNr'] + features + ['Peptide', 'Proteins']].fillna(value=0).to_csv('{}.pin'.format(savepath), sep='\t', index=False)
     return None
-
-def format_output(path_to_pout, search_engine, savepath, fname, fig=True):
-    out = pd.concat([pd.read_csv(path_to_pout, sep='\t'), pd.read_csv(path_to_pout+"_dec", sep='\t')])
-    inp = pd.read_csv(fname + '_all_features.csv')
-
-    if search_engine == "MSGFPlus":
-        out['Label'] = [-1 if p.startswith('XXX') else 1 for p in out.proteinIds]
-        score = 'lnEValue'
-
-    if fig:
-        f, axes = plt.subplots(4, 3, figsize=(16, 21))
-
-        sns.boxplot(data=inp, y=score, x='Label', ax=axes[0][0])
-
-        sns.distplot(inp.loc[inp.Label == -1, score], kde=False, ax=axes[0][1])
-        sns.distplot(inp.loc[inp.Label == 1, score], kde=False, ax=axes[0][1])
-
-        sns.distplot(inp.loc[inp.Label == -1, score], hist=False, ax=axes[0][2])
-        sns.distplot(inp.loc[inp.Label == 1, score], hist=False, ax=axes[0][2])
-
-        sns.boxplot(data=out, y='score', x='Label', ax=axes[1][0])
-
-        sns.distplot(out.loc[out.Label == -1, 'score'], kde=False, ax=axes[1][1])
-        sns.distplot(out.loc[out.Label == 1, 'score'], kde=False, ax=axes[1][1])
-
-        sns.distplot(out.loc[out.Label == -1, 'score'], hist=False, ax=axes[1][2])
-        sns.distplot(out.loc[out.Label == 1, 'score'], hist=False, ax=axes[1][2])
-
-        sns.boxplot(data=out, y='q-value', x='Label', ax=axes[2][0])
-
-        sns.distplot(out.loc[out.Label == -1, 'q-value'], kde=False, ax=axes[2][1])
-        sns.distplot(out.loc[out.Label == 1, 'q-value'], kde=False, ax=axes[2][1])
-
-        sns.distplot(out.loc[out.Label == -1, 'q-value'], hist=False, ax=axes[2][2])
-        sns.distplot(out.loc[out.Label == 1, 'q-value'], hist=False, ax=axes[2][2])
-
-        sns.boxplot(data=out, y='posterior_error_prob', x='Label', ax=axes[3][0])
-
-        sns.distplot(out.loc[out.Label == -1, 'posterior_error_prob'], kde=False, ax=axes[3][1])
-        sns.distplot(out.loc[out.Label == 1, 'posterior_error_prob'], kde=False, ax=axes[3][1])
-
-        sns.distplot(out.loc[out.Label == -1, 'posterior_error_prob'], hist=False, ax=axes[3][2])
-        sns.distplot(out.loc[out.Label == 1, 'posterior_error_prob'], hist=False, ax=axes[3][2])
-
-        axes[0][1].set_xlim([np.min(inp[score]), np.max(inp[score])])
-        axes[0][2].set_xlim([np.min(inp[score]), np.max(inp[score])])
-
-        axes[1][1].set_xlim([np.min(out['score']), np.max(out['score'])])
-        axes[1][2].set_xlim([np.min(out['score']), np.max(out['score'])])
-
-        axes[2][1].set_xlim([np.min(out['q-value']), np.max(out['q-value'])])
-        axes[2][2].set_xlim([np.min(out['q-value']), np.max(out['q-value'])])
-
-        axes[3][1].set_xlim([np.min(out['posterior_error_prob']), np.max(out['posterior_error_prob'])])
-        axes[3][2].set_xlim([np.min(out['posterior_error_prob']), np.max(out['posterior_error_prob'])])
-
-        f.savefig(savepath)
