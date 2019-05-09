72a7bd187d81f651535f51c6a95374d216b0711f
Ralf <RalfG@hotmail.be>
2019-05-09 12:46:16 +0200

    Add more advanced title parser to parse_mgf

--- ms2rescore/parse_mgf.py
+++ ms2rescore/parse_mgf.py
@@ -16,8 +16,46 @@ def get_num_lines(file_path):
     return lines
 
 
+def title_parser(line, method='full'):
+    """
+    Take an MGF TITLE line and return the spectrum title.
+
+    Depending on the software tool that was used to write the MGF files and the
+    search engine, we need to extract different parts of the TITLE field. E.g,
+    for MaxQuant, everything up until the first space is required
+
+    line: string, line from MGF file containing 'TITLE='
+    method: string, one of the following:
+    - 'full': take everything after 'TITLE='
+    - 'first_space': take everything between 'TITLE=' and first space.
+    - 'first_space_no_charge': take everything between 'TITLE=' and first space,
+      but leave out everything after last dot. (required for MaxQuant pipeline).
+    - 'TRFP_MQ': For MGF parsed with ThermoRawFileParser and spec_ids from
+      MaxQuant msms.txt.
+    """
+
+    if method == 'full':
+        title = line[6:].strip()
+    elif method == 'first_space':
+        title = line[6:].split(' ')[0].strip()
+    elif method == 'first_space_no_charge':
+        title = '.'.join(line[6:].split(' ')[0].split('.')[:-1]).strip()
+    elif method == 'TRFP_MQ':
+        line = line.strip().split('mzspec=')[1].split(' ')
+        filename = line[0].replace('.raw:', '')
+        scan = line[3].replace('scan=', '')
+        title = '.'.join([filename, scan, scan])
+    else:
+        raise ValueError("method '{}' is not a valid title parsing method".format(
+            method
+        ))
+    return title
+
+
 def parse_mgf(df_in, mgf_folder, outname='scan_mgf_result.mgf',
-             filename_col='mgf_filename', spec_title_col='spec_id'):
+              filename_col='mgf_filename', spec_title_col='spec_id',
+              title_parsing_method='full',
+              show_progress_bar=True):
 
     if df_in[filename_col].iloc[0][-4:] in ['.mgf', '.MGF']:
         file_suffix = ''
@@ -41,12 +79,10 @@ def parse_mgf(df_in, mgf_folder, outname='scan_mgf_result.mgf',
             id_charges = df_in[(df_in[filename_col] == run)].set_index('spec_id')['charge'].to_dict()
 
             with open(current_mgf_file, 'r') as f:
-                for line in tqdm(f, total=get_num_lines(current_mgf_file)):
+                iterator = tqdm(f, total=get_num_lines(current_mgf_file)) if show_progress_bar else f
+                for line in iterator:
                     if 'TITLE=' in line:
-                        # Take everything between `TITLE=` and the first space
-                        # as the title, but exclude the charge, as the measured
-                        # charge is not always the identified charge...
-                        title = '.'.join(line[6:].split(' ')[0].split('.')[:-1])
+                        title = title_parser(line, method=title_parsing_method)
                         if title in spec_set:
                             found = True
                             line = "TITLE=" + title + "\n"
