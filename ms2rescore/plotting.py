aa4f4e7ffdd723b42242a80610487c93284157ed
Arthur Declercq <arre_declercq@hotmail.com>
2021-08-03 19:49:10 +0200

    reorganizing plotting module

--- ms2rescore/plotting.py
+++ ms2rescore/plotting.py
@@ -13,209 +13,220 @@ from statsmodels.distributions.empirical_distribution import ECDF
 from ms2rescore.percolator import PercolatorIn
 
 
-def target_decoy_distribution(
-    df: pd.DataFrame,
-    q_label: str = "q",
-    score_label: str = "score",
-    decoy_label: str = "is decoy",
-    score_name: str = "Score",
-    fdr_threshold: Optional[float] = None,
-    plot_title: str = ""
-):
-    """
-    Plot target-decoy distributions.
+class _REREC():
+    rerecs = []
 
-    Plot for a given search engine output the target-decoy score distributions, the
-    relation between the q-values and the PSM scores and a PP plot between the target
-    and the decoy distribution.
+    def __init__(self) -> None:
+        self.rerecs.append(self)
 
-    Parameters
-    ----------
-    df : pd.DataFrame
-        Pandas DataFrame containing all rank 1 PSMs
-    q_label : str, optional
-        Label of column containing each PSM's q-value, default `q`
-    score_label : str, optional
-        Label of column containing the search engine scores, default `score`
-    decoy_label : str, optional
-        Label of column marking decoy PSMs as True and target PSMs as False, default
-        `is decoy`
-    score_name : str, optional
-        Score name used in axis labels
-    fdr_threshold : float, optional
-        FDR threshold to plot as vertical dotted line
-    plot_title : str, optional
-        plot suptitle
+    def target_decoy_distribution(
+        self,
+        q_label: str = "q",
+        score_label: str = "score",
+        decoy_label: str = "is decoy",
+        score_name: str = "Score",
+        fdr_threshold: Optional[float] = None,
+        plot_title: str = ""
+    ):
+        """
+        Plot target-decoy distributions.
 
-    Returns
-    -------
-    fig : Matplotlib figure
-    axes : Matplotlib Axes
+        Plot for a given search engine output the target-decoy score distributions, the
+        relation between the q-values and the PSM scores and a PP plot between the target
+        and the decoy distribution.
 
-    """
-    fig, axes = plt.subplots(1, 3, figsize=(16, 4))
+        Parameters
+        ----------
+        df : pd.DataFrame
+            Pandas DataFrame containing all rank 1 PSMs
+        q_label : str, optional
+            Label of column containing each PSM's q-value, default `q`
+        score_label : str, optional
+            Label of column containing the search engine scores, default `score`
+        decoy_label : str, optional
+            Label of column marking decoy PSMs as True and target PSMs as False, default
+            `is decoy`
+        score_name : str, optional
+            Score name used in axis labels
+        fdr_threshold : float, optional
+            FDR threshold to plot as vertical dotted line
+        plot_title : str, optional
+            plot suptitle
 
-    if fdr_threshold:
-        score_cutoff = (
-            df[(df[q_label] <= 0.01) & (~df[decoy_label])]
-            .sort_values(q_label)
-            .iloc[-1][score_label]
-        )
-    else:
-        score_cutoff = None
+        Returns
+        -------
+        fig : Matplotlib figure
+        axes : Matplotlib Axes
 
-    # Score distribution plot
-    plot_list = [
-        list(x)
-        for x in [df[df[decoy_label]][score_label], df[~df[decoy_label]][score_label]]
-    ]
-    axes[0].hist(
-        plot_list,
-        bins=24,
-        label=["Decoy", "Target"],
-        color=["r", "blue"],
-        lw=1,
-        rwidth=1,
-    )
-    if fdr_threshold:
-        axes[0].vlines(
-            x=score_cutoff, ymin=0, ymax=axes[0].get_ylim()[1], linestyles="dashed"
-        )
-    axes[0].legend()
-    axes[0].set_ylabel("Number of matches")
-    axes[0].set_xlabel(score_name)
+        """
+        fig, axes = plt.subplots(1, 3, figsize=(16, 4))
 
-    # Q value plot
-    axes[1].plot(
-        df.sort_values(score_label)[score_label], df.sort_values(score_label)[q_label]
-    )
-    if fdr_threshold:
-        axes[1].vlines(
-            x=score_cutoff, ymin=0, ymax=axes[1].get_ylim()[1], linestyles="dashed"
-        )
-    axes[1].set_ylabel("q-value")
-    axes[1].set_xlabel(score_name)
-
-    # PP plot
-    ratio = df[decoy_label].value_counts()[True] / df[decoy_label].value_counts()[False]
-    Ft = ECDF(df[~df[decoy_label]][score_label])
-    Fd = ECDF(df[df[decoy_label]][score_label])
-    x = df[~df[decoy_label]][score_label]
-    Fdp = Fd(x)
-    Ftp = Ft(x)
-    axes[2].scatter(Fdp, Ftp, s=4)
-    axes[2].plot((0, 1), (0, ratio), color="r")
-    axes[2].set_xlabel("Decoy percentile")
-    axes[2].set_ylabel("Target percentile")
-
-    plt.suptitle(plot_title)
-    sns.despine()
-
-    return fig, axes
-
-
-def qvalue_comparison(
-    datasets: List[pd.DataFrame],
-    dataset_labels: Optional[List[str]] = None,
-    q_label: str = "q",
-    decoy_label: str = "is decoy",
-    fdr_thresholds: Optional[List[float]] = None,
-    log_scale: bool = True,
-    title: str = "",
-    ax: Optional[matplotlib.axes.Axes] = None,
-):
-    """
-    Plot identification count in function of q-value threshold for multiple datasets.
-
-    Parameters
-    ----------
-    datasets : List[pd.DataFrame]
-        list of datasets in the form of `pandas.DataFrame`s
-    dataset_labels : List[str]
-        list of dataset labels to use in figure legend
-    q_label : str, optional
-        label of column containing each PSM's q-value, default `q`
-    decoy_label : str, optional
-        label of column marking decoy PSMs as True and target PSMs as False, default
-        `is decoy`
-    fdr_thresholds : List[float], optional
-        list of FDR thresholds to plot as vertical, dotted lines
-    log_scale : bool
-        plot x-axis (q-values) in log scale or not.
-    ax : matplotlib Axes, optional
-        axes object to draw the plot onto, otherwise uses the current axes.
-
-    Returns
-    -------
-    ax : matplotlib Axes
-
-
-    """
-    if not isinstance(datasets, list):
-        raise TypeError("`datasets` should be of type `list`.")
-    if not datasets:
-        raise ValueError("`datasets` cannot be empty.")
-
-    if not fdr_thresholds:
-        fdr_thresholds = [0.01, 0.001]
-
-    if ax is None:
-        ax = plt.gca()
-
-    max_count = 0
-
-    for i, df_in in enumerate(datasets):
-        # Cumulatively count target IDs at each FDR threshold
-        df = (
-            df_in[~df_in[decoy_label]]
-            .reset_index(drop=True)
-            .sort_values(q_label, ascending=True)
-            .copy()
-        )
-        df["count"] = (~df[decoy_label]).cumsum()
-
-        # Plot counts
-        label = dataset_labels[i] if dataset_labels else None
-        ax.plot(df[q_label], df["count"], label=label, alpha=0.5)
-
-        # Get maximum count, required for vertical line height
-        tmp_max = np.max(df["count"])
-        if tmp_max > max_count:
-            max_count = tmp_max
-
-    # Plot FDR thresholds as dotted lines
-    if fdr_thresholds:
-        for fdr in fdr_thresholds:
-            ax.plot(
-                [fdr] * 2, np.linspace(0, max_count, 2), linestyle="--", color="black",
+        if fdr_threshold:
+            score_cutoff = (
+                self.df[(self.df[q_label] <= 0.01) & (~self.df[decoy_label])]
+                .sort_values(q_label)
+                .iloc[-1][score_label]
             )
+        else:
+            score_cutoff = None
 
-    # Figure labels and legend
-    ax.set_xlim(0.00001, 1)
-    ax.set_ylabel("Number of identified spectra")
-    ax.set_title(title)
-    if log_scale:
-        ax.set_xlabel("FDR threshold (log scale)")
-        ax.set_xscale("log")
-    else:
-        ax.set_xlabel("FDR threshold")
-    if dataset_labels:
+        # Score distribution plot
+        plot_list = [
+            list(x)
+            for x in [self.df[self.df[decoy_label]][score_label], self.df[~self.df[decoy_label]][score_label]]
+        ]
+        axes[0].hist(
+            plot_list,
+            bins=24,
+            label=["Decoy", "Target"],
+            color=["r", "blue"],
+            lw=1,
+            rwidth=1,
+        )
+        if fdr_threshold:
+            axes[0].vlines(
+                x=score_cutoff, ymin=0, ymax=axes[0].get_ylim()[1], linestyles="dashed"
+            )
+        axes[0].legend()
+        axes[0].set_ylabel("Number of matches")
+        axes[0].set_xlabel(score_name)
+
+        # Q value plot
+        axes[1].plot(
+            self.df.sort_values(score_label)[score_label], self.df.sort_values(score_label)[q_label]
+        )
+        if fdr_threshold:
+            axes[1].vlines(
+                x=score_cutoff, ymin=0, ymax=axes[1].get_ylim()[1], linestyles="dashed"
+            )
+        axes[1].set_ylabel("q-value")
+        axes[1].set_xlabel(score_name)
+
+        # PP plot
+        ratio = self.df[decoy_label].value_counts()[True] / self.df[decoy_label].value_counts()[False]
+        Ft = ECDF(self.df[~self.df[decoy_label]][score_label])
+        Fd = ECDF(self.df[self.df[decoy_label]][score_label])
+        x = self.df[~self.df[decoy_label]][score_label]
+        Fdp = Fd(x)
+        Ftp = Ft(x)
+        axes[2].scatter(Fdp, Ftp, s=4)
+        axes[2].plot((0, 1), (0, ratio), color="r")
+        axes[2].set_xlabel("Decoy percentile")
+        axes[2].set_ylabel("Target percentile")
+
+        plt.suptitle(plot_title)
+        sns.despine()
+
+        return fig, axes
+
+    def qvalue_comparison(
+        cls,
+        dataset_labels: Optional[List[str]] = None,
+        q_label: str = "q",
+        decoy_label: str = "is decoy",
+        fdr_thresholds: Optional[List[float]] = None,
+        log_scale: bool = True,
+        title: str = "",
+        ax: Optional[matplotlib.axes.Axes] = None,
+    ):
+        """
+        Plot identification count in function of q-value threshold for multiple datasets.
+
+        Parameters
+        ----------
+        datasets : List[pd.DataFrame]
+            list of datasets in the form of `pandas.DataFrame`s
+        dataset_labels : List[str]
+            list of dataset labels to use in figure legend
+        q_label : str, optional
+            label of column containing each PSM's q-value, default `q`
+        decoy_label : str, optional
+            label of column marking decoy PSMs as True and target PSMs as False, default
+            `is decoy`
+        fdr_thresholds : List[float], optional
+            list of FDR thresholds to plot as vertical, dotted lines
+        log_scale : bool
+            plot x-axis (q-values) in log scale or not.
+        ax : matplotlib Axes, optional
+            axes object to draw the plot onto, otherwise uses the current axes.
+
+        Returns
+        -------
+        ax : matplotlib Axes
+
+
+        """
+        if not isinstance(cls.rerecs, list):
+            raise TypeError("`datasets` should be of type `list`.")
+        if not cls.rerecs:
+            raise ValueError("`datasets` cannot be empty.")
+
+        if not fdr_thresholds:
+            fdr_thresholds = [0.01, 0.001]
+
+        if ax is None:
+            ax = plt.gca()
+
+        max_count = 0
+
+        for i, rerec in enumerate(cls.rerecs):
+            # Cumulatively count target IDs at each FDR threshold
+            df = (
+                rerec.df[~rerec.df[decoy_label]]
+                .reset_index(drop=True)
+                .sort_values(q_label, ascending=True)
+                .copy()
+            )
+            df["count"] = (~df[decoy_label]).cumsum()
+
+            # Plot counts
+            if dataset_labels:
+                label = dataset_labels[i]
+            else:
+                label = rerec.rescore_features
+            ax.plot(df[q_label], df["count"], label=label, alpha=0.5)
+
+            # Get maximum count, required for vertical line height
+            tmp_max = np.max(df["count"])
+            if tmp_max > max_count:
+                max_count = tmp_max
+
+        # Plot FDR thresholds as dotted lines
+        if fdr_thresholds:
+            for fdr in fdr_thresholds:
+                ax.plot(
+                    [fdr] * 2, np.linspace(0, max_count, 2), linestyle="--", color="black",
+                )
+
+        # Figure labels and legend
+        ax.set_xlim(0.00001, 1)
+        ax.set_ylabel("Number of identified spectra")
+        ax.set_title(title)
+        if log_scale:
+            ax.set_xlabel("FDR threshold (log scale)")
+            ax.set_xscale("log")
+        else:
+            ax.set_xlabel("FDR threshold")
+
         ax.legend()
 
-    return ax
+        return ax
 
 
-def plot_rescoring_results(
-    path_to_pin,
-    path_to_target_pout,
-    path_to_decoy_pout
-):
+class PIN(_REREC):
 
-    def _read_pin_file(path_to_pin):
-        pin = PercolatorIn("Linfeng_010411_HapMap31.pin")
+    def __init__(self, path_to_pin, score_metric) -> None:
+        super().__init__()
+        self.df = self._read_pin_file(path_to_pin)
+        self.score_metric = score_metric
+        self.type = "pout"
+        self.rescore_features = "Before rescoring"
+
+    def _read_pin_file(self, path_to_pin):
+        pin = PercolatorIn(path_to_pin)
         pin_qvalues = pd.DataFrame(qvalues(
             pin.df,
-            key=pin.df["lnEValue"],
+            key=pin.df[self.score_metric],
             is_decoy=pin.df["Label"] == -1,
             reverse=True,
             remove_decoy=False,
@@ -223,17 +234,30 @@ def plot_rescoring_results(
         ))
         return pin_qvalues
 
-    def _read_pout_file(path_to_target_pout, path_to_decoy_pout):
-        """Read target and decoy pout files and combine into single pandas DataFrame."""
-        def _read_pout(path):
-            pout = pd.read_csv(
-                PercolatorIn.fix_tabs(path, id_column="PSMId"),
-                sep="\t"
-            )
-            return pout
 
-        target_pout = _read_pout(path_to_target_pout)
-        decoy_pout = _read_pout(path_to_decoy_pout)
+class POUT(_REREC):
+
+    def __init__(self, path_to_target_pout, path_to_decoy_pout_, rescoring_features) -> None:
+        super().__init__()
+        self.target_pout = path_to_target_pout
+        self.decoy_pout = path_to_decoy_pout_
+        self.df = self._read_pout_file
+        self.type = "pout"
+        self.rescore_features = "After rescoring: " + " ".join(rescoring_features)
+
+    @staticmethod
+    def _read_pout(path):
+        pout = pd.read_csv(
+            PercolatorIn.fix_tabs(path, id_column="PSMId"),
+            sep="\t"
+        )
+        return pout
+
+    def _read_pout_file(self):
+        """Read target and decoy pout files and combine into single pandas DataFrame."""
+
+        target_pout = self._read_pout(self.target_pout)
+        decoy_pout = self._read_pout(self.decoy_pout)
         target_pout["Label"] = 1
         decoy_pout["Label"] = -1
         pout = pd.concat([target_pout, decoy_pout])
@@ -244,22 +268,3 @@ def plot_rescoring_results(
         pout_qvalues["is decoy"] = pout["Label"] == -1
 
         return pout_qvalues
-
-    pin_qvalues = _read_pin_file(path_to_pin)
-    pout_qvalues = _read_pout_file(path_to_target_pout, path_to_decoy_pout)
-
-    fig, axes = target_decoy_distribution(
-        pin_qvalues, plot_title="Before rescoring"
-    )
-    plt.savefig("target_decoy_dist_before_rescoring.svg")
-
-    fig, axes = target_decoy_distribution(
-        pout_qvalues, plot_title="After rescoring"
-    )
-    plt.savefig("target_decoy_dist_after_rescoring.svg")
-
-    ax = qvalue_comparison(
-        datasets=[pin_qvalues, pout_qvalues],
-        dataset_labels=["Before rescoring", "After rescoring"],
-    )
-    plt.savefig("qvalue_comparison.svg")
