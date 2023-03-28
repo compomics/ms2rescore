d01559a371b08eb2427fecfb3a1d195caa5eac1c
ArthurDeclercq <arre_declercq@hotmail.com>
2023-03-28 12:13:20 +0200

    parse mgf fixes

--- ms2rescore/parse_mgf.py
+++ ms2rescore/parse_mgf.py
@@ -6,6 +6,7 @@ import os.path
 from typing import Union, Tuple, Dict
 
 from rich.progress import track
+from pyteomics.mgf import Mgf
 
 from ms2rescore.exceptions import MS2RescoreError
 
@@ -22,34 +23,23 @@ def parse_mgf_title_rt(
 ) -> Dict[str, float]:
     """Parse MGF file to extract title and retention time fields, by spectrum index."""
     logger.debug("Parsing MGF file to extract retention times.")
+    mgf_reader = Mgf(path_to_mgf, read_charges=False, read_ions=False)
+    retention_times = {}
+    for spectrum in mgf_reader:
+        try:
+            title = spectrum["params"]["title"]
+        except KeyError:
+            raise ParseMGFError("MGF file missing title field.")
+        try:
+            rt = float(spectrum["params"]["rtinseconds"])
+        except KeyError:
+            rt = None
+        retention_times[title] = rt
 
-    title = None
-    retention_time = None
-    retention_times = dict()
-    spectrum_header = False
-    with open(path_to_mgf, "rt") as mgf_in:
-        for line in mgf_in:
-            if not line[0].isdigit():
-                spectrum_header = True
-                if line[0] == "T":
-                    if line.startswith("TITLE="):
-                        title = line[6:].strip()
-                elif line[0] == "R":
-                    if line.startswith("RTINSECONDS="):
-                        retention_time = float(line[12:].strip())
-            elif line[0].isdigit() and spectrum_header:
-                if title is None:
-                    raise ParseMGFError("Missing `TITLE` for `RTINSECONDS` entry.")
-                if title is None:
-                    raise ParseMGFError("Missing `RTINSECONDS` for `TITLE` entry.")
-                retention_times[title] = retention_time
-                title = None
-                spectrum_header = False
-            else:
-                raise ParseMGFError("Expected spectrum header for spectrum")
-
-    return retention_times
-
+        if any(list(retention_times.values())):
+            return retention_times
+        else:
+            raise ParseMGFError("MGF file missing rtinseconds field.")
 
 def get_num_lines(file_path):
     fp = open(file_path, "r+")
