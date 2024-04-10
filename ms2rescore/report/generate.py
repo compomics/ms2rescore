1999039d55ad0bfb9178431dfbeb386ac0121a75
ArthurDeclercq <arre_declercq@hotmail.com>
2024-04-10 13:42:53 +0200

    Apply suggestions from code review :recycle:
    :recycle:
    
    Co-authored-by: Ralf Gabriels <ralfg@hotmail.be>

--- ms2rescore/report/generate.py
+++ ms2rescore/report/generate.py
@@ -180,13 +180,12 @@ def _get_stats_context(confidence_before, confidence_after):
         if not before or not after:
             continue
         increase = (after - before) / before * 100
-        diff = after - before
         stats.append(
             {
                 "item": level_name,
                 "card_color": card_color,
                 "number": after,
-                "diff": f"({diff:+})",
+                "diff": f"({after - before:+})",
                 "percentage": f"{increase:.1f}%",
                 "is_increase": increase > 0,
                 "bar_percentage": before / after * 100 if increase > 0 else after / before * 100,
