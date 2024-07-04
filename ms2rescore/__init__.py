a0b9ce451434e74f46960be6081504351bd67bd1
RalfG <ralfg@hotmail.be>
2024-07-04 19:29:42 +0200

    Version bump

--- ms2rescore/__init__.py
+++ ms2rescore/__init__.py
@@ -1,6 +1,6 @@
 """MS²Rescore: Sensitive PSM rescoring with predicted MS² peak intensities and RTs."""
 
-__version__ = "3.1.0-dev6"
+__version__ = "3.1.0-dev7"
 
 from warnings import filterwarnings
 
